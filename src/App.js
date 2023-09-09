15c28cad9f8e3a1810afac5e5b51e170894ddf08
Max Lisicin <maxlisicin@Maxs-MacBook-Air.local>
2023-09-09 20:29:28 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.scss";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickMinus = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  const onClickPlus = () => {
+    setCount((prev) => prev + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">
+          - Минус
+        </button>
+        <button onClick={onClickPlus} className="plus">
+          Плюс +
+        </button>
+      </div>
     </div>
   );
 }
